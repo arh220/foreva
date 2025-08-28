90ab021998bbfb65ce60cc28d674ea32d0c762f0
Aradhana Kathrotiya <sojitra98aradhana@gmail.com>
2025-08-28 18:44:12 +0530

    host

--- controllers/user/allitem.js
+++ controllers/user/allitem.js
@@ -3,6 +3,7 @@ const Item = require("../../models/admin/items");
 
 async function getAllItem(req, res) {
   const catId = await Category.findById(req.params.id);
+  console.log("selectItem", catId);
   const selectItem = await Item.find({ catid: catId }).populate("catid");
   res.render("items", { selectItem });
 }
